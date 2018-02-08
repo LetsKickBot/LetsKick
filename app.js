d254c07d5631c4c0561164325be7e07df7c69df6
Tuan <atuannguyen1101@gmail.com>
2018-02-08 11:11:25 -0500

    Added handleMesage and SendAPI

--- app.js
+++ app.js
@@ -3,40 +3,52 @@
 
 // Imports dependencies and set up http server
 const
+  request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
-  Data = require('./utils/get_data')
+  Data = require('./utils/get_data');
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => {
   console.log('webhook is listening')
-  Data.get_next_game('Manchester United', (response) => {
-    console.log(response);
-  })
 });
 
 // Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {  
 
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      let user_message = webhook_event.message.text;
+      
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
@@ -46,7 +58,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "hokola";
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
 
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -69,3 +81,56 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  console.log(received_message.text);
+  Data.get_next_game(received_message.text, (reply) => {
+      if (received_message.text) {    
+        let date = new Date(reply[2]);
+        let newDate = ("" + date);
+      // Create the payload for a basic text message
+      response = {
+        "text": `${reply[0]} will play again ${reply[1]} on ${newDate}`
+      }
+    }  
+  
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+  })
+    
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+
+    // Try to add the access_token to the enviromental variable instead of embedding it into the code like this.
+    "qs": { "access_token": 'EAACChvEnROQBAFZA0OZBs4tBRU8oeTVtNSl2TbmIkikmGUZAWFddfJVIRSzMui4qEzskD5VljnrpYgbCR0KULaKPXwD7vPjLQ4X3WgsH9bvjy6LIkjYY4ZBZAZCwVnZBULNAj5sqBOYT4p8A5XklNXETYLFt5cfauKgAgTgTMaSZCOjhJOmYiwca' },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
+function handlePostback (sender_psid, received_postback) {
+
+}
