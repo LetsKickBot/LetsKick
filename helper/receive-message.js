ff7a788efe0fd6d116fdc803722816fca05525c6
baonguyen2604 <quocbaonguyencong@gmail.com>
2018-02-13 23:09:19 -0600

    [FrontEnd] fix errors

--- helper/receive-message.js
+++ helper/receive-message.js
@@ -1,31 +1,31 @@
 const
-  	bodyParser = require('body-parser'),
-  	request = require('request'),
-	Data = require('./utils/get_data');
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  Data = require('../data/get_data');
 
-const handleMessage(sender_psid, received_message) {
+const handleMessage = (sender_psid, received_message) => {
 
   let response;
 
   // Check if the message contains text
   console.log(received_message.text);
   Data.get_next_game(received_message.text, (reply) => {
-      if (received_message.text) {    
-        let date = new Date(reply[2]);
-        let newDate = ("" + date);
+      if (received_message.text) {
+        let date = reply[2];
+        let time = reply[3];
+        let info = reply[4];
       // Create the payload for a basic text message
       response = {
-        "text": `${reply[0]} will play again ${reply[1]} on ${newDate}`
+        "text": `${reply[0]} will play against ${reply[1]} on ${date}, ${time}, for ${info}`
       }
-    }  
-  
+    }
+
     // Sends the response message
     callSendAPI(sender_psid, response);
   })
-    
 }
 
-const callSendAPI(sender_psid, response) {
+const callSendAPI = (sender_psid, response) => {
   // Construct the message body
   let request_body = {
     "recipient": {
@@ -48,7 +48,7 @@ const callSendAPI(sender_psid, response) {
     } else {
       console.error("Unable to send message:" + err);
     }
-  }); 
+  });
 }
 
 function handlePostback (sender_psid, received_postback) {
