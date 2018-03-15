b7424b0e4b6bba4259d48ad399e25496bb67eb81
baonguyen2604 <quocbaonguyencong@gmail.com>
2018-03-14 23:14:41 -0500

    [Backend] Fixed 'pre strip' bug and time bug

--- helper/receive-message.js
+++ helper/receive-message.js
@@ -55,7 +55,7 @@ const callSendAPI = (sender_psid, response) => {
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v2.6",
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
 
     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
     "method": "POST",
