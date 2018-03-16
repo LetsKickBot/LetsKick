0ce02f9e8f9604f0b4f56ce53e5855f67992023d
tnguyen452 <tnntech@gatech.edu>
2018-03-16 01:34:49 -0400

    update

--- helper/function.js
+++ helper/function.js
@@ -6,30 +6,35 @@ function checkSpellName(name) {
 	var identityTeam = []
 	name = name.replace(/\s/g,'').toUpperCase();
 	for (var key in file) {
-		array = file[key]
-		team = key.replace(/\s/g,'').toUpperCase()
-		// Check to see if the specific team name has duplicate with other team name at diffrent location
-		if ((name.length >= 4) && team.indexOf(name) !== -1) {
-			identityTeam.push(key)
-		}
-		for (var i in array) {
-			// Check to see if the team name same as shortcut name
-			if (name == array[i].replace(/\s/g,'').toUpperCase()) {
-				correctTeam = key
-				break
-			//	Check to see if the specific team name has duplicate with other team name at same location
-			} else if (name == array[i].replace(/\s/g,'').substring(0, name.length).toUpperCase()) {
-				if (identityTeam.includes(key) == false) {
-					identityTeam.push(key)
+		if (flag) {
+			array = file[key]
+			team = key.replace(/\s/g,'').toUpperCase()
+			// Check to see if the specific team name has duplicate with other team name at diffrent location
+			if ((name.length >= 4) && team.indexOf(name) !== -1) {
+				identityTeam.push(key)
+			}
+			for (var i in array) {
+				// Check to see if the team name same as shortcut name
+				if (name == array[i].replace(/\s/g,'').toUpperCase()) {
+					correctTeam = key
+					flag = false
+					break
+				//	Check to see if the specific team name has duplicate with other team name at same location
+				} else if (name == array[i].replace(/\s/g,'').substring(0, name.length).toUpperCase()) {
+					if (identityTeam.includes(key) == false) {
+						identityTeam.push(key)
+					}
 				}
 			}
+		} else {
+			break
 		}
 	}
-
-	//Final Check
-	if (identityTeam.length == 0 || correctTeam !== '')
-		return correctTeam
+	//Final Check		
 	switch(identityTeam.length) {
+		case 0:
+			return correctTeam
+			break
 		case 1:
 			return identityTeam[0];
 			break;
