2350ec1b29f4decaffb00469b7badacf69c8f924
tnguyen452 <tnntech@gatech.edu>
2018-03-15 22:59:44 -0400

    update

--- app.js
+++ app.js
@@ -3,19 +3,24 @@
 
 // Imports dependencies and set up http server
 const
+	// func = require('./helper/function.js'),
   request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()), // creates express http server
+  app = express(),
   webhooks = require('./routes/webhooks');
 
+  
 // Sets server port and logs message on success
+// process.env.PORT || 
 app.listen(process.env.PORT || 1337, () => {
-  console.log('webhook is listening')
+
+  console.log('webhook is listening');
+
 });
 
-app.use('/webhooks', webhooks)
+app.use(bodyParser.json());
+
+app.use('/webhook', webhooks);
 
 module.exports = app;
-
-
