d2a5d27ad313e3b007cc8ed4e651a4abdf27936e
tnguyen452 <tnntech@gatech.edu>
2018-03-16 23:13:58 -0400

    dasda

--- helper/receive-message.js
+++ helper/receive-message.js
@@ -63,66 +63,13 @@ const handleMessage = (sender_psid, received_message) => {
   }
 }
 
-// const callSendAPI = (sender_psid, response) => {
-
-//     let request_body = {
-//     "recipient": {
-//       "id": sender_psid
-//     },
-//     "message": response
-//     }
-//   // Send the HTTP request to the Messenger Platform
-//   request({
-//     "uri": "https://graph.facebook.com/v2.6/me/messages",
-//     // "uri": "http://localhost:3100/v2.6",
-//     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
-//     "method": "POST",
-//     "json": request_body
-//   }, (err, res, body) => {
-//     if (err) {
-//       console.error("Unable to send message:" + err);
-//     }
-//   });
-// }
-
 const callSendAPI = (sender_psid, response) => {
 
     let request_body = {
     "recipient": {
       "id": sender_psid
     },
-    "message": {
-      "attachment":{
-        "payload":{
-          "template_type":"generic",
-          "elements":[
-          {
-            "title":"Welcome!",
-            "image_url":"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We have the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://petersfancybrownhats.com/view?item=103",
-              "messenger_extensions": false,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://petersfancybrownhats.com/"
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }
-            ]
-          }
-          ]
-        }
-      }
-    }
+    "message": response
     }
   // Send the HTTP request to the Messenger Platform
   request({
@@ -137,6 +84,59 @@ const callSendAPI = (sender_psid, response) => {
     }
   });
 }
+
+// const callSendAPI = (sender_psid, response) => {
+
+//     let request_body = {
+//     "recipient": {
+//       "id": sender_psid
+//     },
+//     "message": {
+//       "attachment":{
+//         "payload":{
+//           "template_type":"generic",
+//           "elements":[
+//           {
+//             "title":"Welcome!",
+//             "image_url":"https://petersfancybrownhats.com/company_image.png",
+//             "subtitle":"We have the right hat for everyone.",
+//             "default_action": {
+//               "type": "web_url",
+//               "url": "https://petersfancybrownhats.com/view?item=103",
+//               "messenger_extensions": false,
+//               "webview_height_ratio": "tall",
+//               "fallback_url": "https://petersfancybrownhats.com/"
+//             },
+//             "buttons":[
+//               {
+//                 "type":"web_url",
+//                 "url":"https://petersfancybrownhats.com",
+//                 "title":"View Website"
+//               },{
+//                 "type":"postback",
+//                 "title":"Start Chatting",
+//                 "payload":"DEVELOPER_DEFINED_PAYLOAD"
+//               }
+//             ]
+//           }
+//           ]
+//         }
+//       }
+//     }
+//     }
+//   // Send the HTTP request to the Messenger Platform
+//   request({
+//     "uri": "https://graph.facebook.com/v2.6/me/messages",
+//     // "uri": "http://localhost:3100/v2.6",
+//     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
+//     "method": "POST",
+//     "json": request_body
+//   }, (err, res, body) => {
+//     if (err) {
+//       console.error("Unable to send message:" + err);
+//     }
+//   });
+// }
 
 
 const autoQuickReply = (sender_psid, value) => {
